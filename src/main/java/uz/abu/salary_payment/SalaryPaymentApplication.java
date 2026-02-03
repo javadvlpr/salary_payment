7f313d1ae44db2988be9900c3664ded2e0f66340
Abdurahmon <abdurahmonimomnazarov543@gmail.com>
2026-02-03 13:12:44 +0500

    Porject created with Spring framework

---
+++ src/main/java/uz/abu/salary_payment/SalaryPaymentApplication.java
@@ -0,0 +1,13 @@
+package uz.abu.salary_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/abu/salary_payment/SalaryPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.abu.salary_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
